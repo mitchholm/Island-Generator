7d679d0033bdd4ae27f75c7dca2aca81412607c0
mholmPurdue <mholm@purdue.edu>
2014-05-12 13:29:48 -0400

    Preview image and nargs update

--- nargs.c
+++ nargs.c
@@ -4,26 +4,24 @@
 
 //Returns the index of the argument if its present, otherwise 0
 int isPresent(int argc, char** argv, char* test){
-	for(int i = 1; i < argc; ++i){
+	for(int i = 1; i < argc; ++i)
 		if(!strncmp(&argv[i][1], test, strlen(test)) &&
 		 		(argv[i][strlen(test)+1] == '=' || argv[i][strlen(test)+1] == '\0'))
 			return i;
-	}
 	return 0;
 }
 
 //Returns the index of the part of the argument after the equal sign
 char* argString(int argc, char** argv, char* test){
-	for(int i = 1; i < argc; ++i){
+	for(int i = 1; i < argc; ++i)
 		if(!strncmp(&argv[i][1], test, strlen(test)) && argv[i][strlen(test)+1] == '=')
 			return &argv[i][strlen(test) + 2];
-	}
 	return 0;
 }
 
 //Returns the double after the equal sign 
 double argDouble(int argc, char** argv, char* test){
-	for(int i = 1; i < argc; ++i){
+	for(int i = 1; i < argc; ++i)
 		if(!strncmp(&argv[i][1], test, strlen(test)) &&
 		 		(argv[i][strlen(test)+1] == '=' || argv[i][strlen(test)+1] == '\0')){
 			char* error;
@@ -32,18 +30,14 @@ double argDouble(int argc, char** argv, char* test){
 				return temp;
 			else
 				continue;
-		}
-			
-	}
+		}	
 	return 0;
 }
 
 int argInt(int argc, char** argv, char* test){
-	for(int i = 1; i < argc; ++i){
+	for(int i = 1; i < argc; ++i)
 		if(!strncmp(&argv[i][1], test, strlen(test)) &&
 		 		(argv[i][strlen(test)+1] == '=' || argv[i][strlen(test)+1] == '\0'))
 			return atoi(&argv[i][strlen(test) +2]);
-			
-	}
 	return 0;
 }
